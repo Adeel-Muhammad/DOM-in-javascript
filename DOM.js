ca560e029f0f5084beef188c47813cd297b2618d
Adeel-Muhammad <126050873+Adeel-Muhammad@users.noreply.github.com>
2023-08-31 02:58:51 +0500

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,38 @@
+var producn = document.getElementById("product-Name");
+var productpp = document.getElementById("product-price");
+var productDate = document.getElementById("product-date");
+var productdes= document.getElementById("product-description");
+
+
+// var tabl = document.getElementById("tabl");
+// var row = document.createElement("tr");
+// var clm1 = document.createElement("td");
+// var clm2 = document.createElement("td");
+// var clm3 = document.createElement("td");
+// var clm4 = document.createElement("td");
+var addBtn=document.getElementById("expense-btn");
+
+// var value =addBtn.addEventListener("click",function(){
+//     var newrow =tabl.appendChild(row);
+// var newclm1=row.appendChild(clm1);
+// var newclm2=row.appendChild(clm2);
+// var newclm3=row.appendChild(clm3);
+// var newclm4=row.appendChild(clm4);
+// function values(){
+// newclm1.innerText=producn.value;
+// newclm2.innerText=productpp.value;
+// newclm3.innerText=productDate.value;
+// newclm4.innerText=productdes.value;
+// }
+
+
+
+
+
+})
+
+
+
+
+
+
